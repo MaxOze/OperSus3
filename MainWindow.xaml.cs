e5365b8c98342e4cb1aa3bbd894017eb4725625c
max <2002ozerov@mail.ru>
2022-12-14 16:21:56 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Mutex mutex = new Mutex();
+        private ObservableCollection<Process> processesList;
+        private bool run = true;
+        private Process executedProcess = null;
+        private Func<Process, Process, int> sorting = null;
+        public MainWindow()
+        {
+            this.DataContext = this;
+            processesList = new ObservableCollection<Process>();
+
+            Thread executingThread = new Thread(() => ExecuteProcess());
+            Thread showThread = new Thread(() => ShowExecutedProcess());
+            Thread timeThread = new Thread(() => TimeChange());
+            InitializeComponent();
+            ProcessList.ItemsSource = processesList;
+
+            sorting = Process.sortPlan;
+
+            executingThread.Start();
+            showThread.Start();
+            timeThread.Start();
+
+        }
+
+        private void AddProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int CP = Convert.ToInt32(AllocatedCPText.Text);
+                int memory = Convert.ToInt32(AllocatedMemoryText.Text);
+                Process process = new Process(CP, memory);
+                processesList.Add(process);
+                sortAfterAdd(sorting);
+            }
+            catch
+            {
+
+            }
+
+            AllocatedCPText.Text = "";
+            AllocatedMemoryText.Text = "";
+        }
+
+        private void sortAfterAdd(Func<Process, Process, int> sorting)
+        {
+            if (processesList.Count > 1)
+            {
+                mutex.WaitOne();
+
+
+                bool isSorted;
+
+                do
+                {
+                    isSorted = true;
+
+                    for (var i = 0; i < processesList.Count - 1; i++)
+                    {
+                        if (sorting(processesList[i], processesList[i + 1]) > 0)
+                        {
+                            (processesList[i], processesList[i + 1]) = (processesList[i + 1], processesList[i]);
+                            isSorted = false;
+                        }
+                    }
+                } while (isSorted != true);
+
+
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void ExecuteProcess()
+        {
+            while (true)
+            {
+                if (run)
+                {
+                    if (processesList.Count != 0 || executedProcess != null)
+                    {
+                        if (executedProcess != null)
+                        {
+                            executedProcess.executeTime--;
+                            if (executedProcess.executeTime == 0)
+                                executedProcess = null;
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            mutex.WaitOne();
+                            executedProcess = processesList.First();
+                            Dispatcher.Invoke(() => processesList.RemoveAt(0));
+                            mutex.ReleaseMutex();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ShowExecutedProcess()
+        {
+            while (true)
+            {
+                mutex.WaitOne();
+                if (executedProcess != null)
+                {
+                    Dispatcher.Invoke(() => CurrentProcessText.Text = executedProcess.ToString());
+                }
+                else
+                {
+                    Dispatcher.Invoke(() => CurrentProcessText.Text = "Ожидание");
+                }
+                mutex.ReleaseMutex();
+                Thread.Sleep(500);
+            }
+        }
+
+        private void TimeChange()
+        {
+            while (true)
+            {
+                Dispatcher.Invoke(() => TimeBlock.Text = DateTime.Now.ToLongTimeString());
+                if (Dispatcher.Invoke(() => processesList.Count > 0))
+                {
+                    mutex.WaitOne();
+                    foreach (var process in processesList)
+                    {
+                        process.waitingTime++;
+                    }
+                    Dispatcher.Invoke(() => ProcessList.Items.Refresh());
+                    mutex.ReleaseMutex();
+                }
+
+                if (Dispatcher.Invoke(() => ButtonHRN.IsChecked == true))
+                    Dispatcher.Invoke(() => sortAfterAdd(sorting));
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Alghoritm_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            if (pressed.Content.Equals("Plan"))
+                sorting = Process.sortPlan;
+            else
+                sorting = Process.sortHRN;
+
+            sortAfterAdd(sorting);
+        }
+
+        private void Run_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            if (pressed.Content.Equals("Работает"))
+                run = true;
+            else
+                run = false;
+        }
+
+        private void DeleteProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.Invoke(() => processesList.Remove((Process)ProcessList.SelectedItem));
+            sortAfterAdd(sorting);
+        }
+    }
+}
---
+++ Process.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    public class Process
+    {
+        private static int nextID = 0;
+        public int Id { get; set; }
+        public int AllocatedCP { get; set; }
+        public int AllocatedMemory { get; set; }
+        public DateTime creationTime { get; set; }
+        public int executeTime { get; set; }
+        public int waitingTime { get; set; }
+        public int priorityLvl { get; set; }    
+
+        public Process(int CP, int Memory) 
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            Id = ++nextID;
+            AllocatedCP = CP;    
+            AllocatedMemory = Memory;    
+            creationTime = DateTime.Now;
+            executeTime = rnd.Next(10,30);
+            priorityLvl = rnd.Next(1,10);
+        }
+
+        public override string ToString()
+        {
+            return $"№{Id} <{creationTime:T}> -{AllocatedCP}-/-{AllocatedMemory}- >> {executeTime}";
+        }
+
+        public static int sortPlan(Process process1, Process process2)
+        {
+            return process1.priorityLvl - process2.priorityLvl; 
+        }
+
+        public static int sortHRN(Process process1, Process process2)
+        {
+            double first = ((double)process1.waitingTime + (double)process1.executeTime) / (double)process1.executeTime;
+            double second = ((double)process2.waitingTime + (double)process2.executeTime) / (double)process2.executeTime;
+
+            if (first > second)
+                return 1;
+            else if (first < second)
+                return -1;
+            else return 0;
+        }
+    }
+}
